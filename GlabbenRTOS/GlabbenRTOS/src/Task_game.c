74e5ca486b49e6202ac3bed122f4b51c17e9ae79
tnt04168 <andre.ahlbertz@gmail.com>
2019-01-30 15:40:15 +0100

    While loopar gjorda

--- GlabbenRTOS/GlabbenRTOS/src/Task_game.c
+++ GlabbenRTOS/GlabbenRTOS/src/Task_game.c
@@ -39,37 +39,53 @@ void task_game(void *pvParameters){
 	{
 		
 		//printf("inside while in game");
-		
-		if (readLCDbutton() == btnSELECT && !ioport_get_pin_level(BUTTON_INPUT_01) && !ioport_get_pin_level(BUTTON_INPUT_02))	
-		{	
-			if (!game_started)
+		while (!game_started && !ioport_get_pin_level(BUTTON_INPUT_01) && !ioport_get_pin_level(BUTTON_INPUT_02))
+		{
+			if (readLCDbutton() == btnSELECT)
 			{
-				
 				game_started = 1;
-					
+				
 				lcdClearDisplay();
-					
+				
 				ioport_set_pin_level(LED_OUTPUT, LOW);
-					
-					//Show game start on display
 				printf("Game start \n");
-					
-					//Randomized time
-					//uint32_t randomTime = trng_read_output_data(&myTrng)%5000000 + 2000000;		// microseconds
-// 					uint32_t randomTime = 2000000;		// microseconds
-// 					printf("%lu\n", randomTime);
-					
+				
 				uint32_t count = (trng_read_output_data(TRNG) % 5000000) + 2000000;
 				printf("Time in loop: %lu \n",count);
-					
-					//Start timer
-					//delayMicroseconds(randomTime);
-					
-				delayMicroseconds(timer_time);
-					
-				ioport_set_pin_level(LED_OUTPUT, HIGH);
-					
 				
+				
+			}
+		}
+		
+		while (game_started)
+		{
+			delayMicroseconds(timer_time);
+			
+			ioport_set_pin_level(LED_OUTPUT, HIGH);
+			
+			
+			if(ioport_get_pin_level(BUTTON_INPUT_01) && !button1_started)				//
+			{
+				//button1_started = 1;
+				if (xSemaphoreGive(button1) == pdTRUE)
+				{
+					button1_test = 1;
+					printf("Sem1 given \n");
+				}
+				vTaskDelay(100);
+
+			}
+			
+			if(ioport_get_pin_level(BUTTON_INPUT_02) && !button2_started)				//
+			{
+				//button2_started = 1;
+				if (xSemaphoreGive(button2) == pdTRUE)
+				{
+					button2_test = 1;
+					printf("Sem2 given \n");
+				}
+				vTaskDelay(100);
+
 				
 			}
 			
@@ -78,30 +94,7 @@ void task_game(void *pvParameters){
 		
 		
 		
-		if(ioport_get_pin_level(BUTTON_INPUT_01) && game_started && !button1_started)				//
-		{
-			//button1_started = 1;
-			if (xSemaphoreGive(button1) == pdTRUE)
-			{
-				button1_test = 1;
-				printf("Sem1 given \n");
-			}
-			vTaskDelay(100);
-
-		}
 		
-		if(ioport_get_pin_level(BUTTON_INPUT_02) && game_started && !button2_started)				//
-		{
-			//button2_started = 1;
-			if (xSemaphoreGive(button2) == pdTRUE)
-			{
-				button2_test = 1;
-				printf("Sem2 given \n");
-			}
-			vTaskDelay(100);
-
-			
-		}
 		
 // 		uint32_t button_register = PIOC->PIO_PDSR;
 // 		if ((button_register & (1<<8)) && (button_register & (1<<17)))
